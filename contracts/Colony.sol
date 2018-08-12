95f6d240579a9f940c810ede15c9196397b3edf7
Stephen Jackson <stevejaxon@users.noreply.github.com>
2018-08-12 10:29:09 +0100

    Refactoring to removing the old code
    Moved the resource code out to its own contract

--- contracts/Colony.sol
+++ contracts/Colony.sol
@@ -9,52 +9,5 @@ pragma solidity ^0.4.24;
  * Note **This is not production ready** - it's still very much a rough draft to experiment with an idea. 
  */
 contract Colony {
-    struct Resource {
-        uint amount;
-        uint8 blockNumber;
-        uint8 level;
-        uint8 rate;
-        uint8 resourceType;
-    }
-    
-    uint8[] public levelRates;
-    uint8[] public upgadeCosts;
-    Resource public resource;
-    
-    constructor(uint8[] _levelRates, uint8[] _upgradeCosts) public {
-        require(_levelRates.length == _upgradeCosts.length);
-        require(_upgradeCosts[0] == 0);
-        levelRates = _levelRates;
-        upgadeCosts = _upgradeCosts;
-    }
-    
-    function constructResource() external {
-        // assert(resource.blockNumber == 0);
-        resource = Resource(0, uint8(block.number + 1), 0, levelRates[0], 0);
-    }
-    
-    function upgradeResource() external {
-        require(resource.blockNumber != 0);
-        uint available = _calculateAvailableResources();
-        uint8 nextLevel = resource.level + 1;
-        uint8 cost = levelRates[nextLevel];
-        require(available >= cost);
-        resource.amount = available - cost;
-        resource.blockNumber = uint8(block.number);
-        resource.level = nextLevel;
-        resource.rate = levelRates[resource.level];
-    }
-    
-    function getAvailableResource() constant external returns(uint _amount) {
-        if (resource.blockNumber == 0) {
-            _amount = 0; 
-        } else {
-            _amount = _calculateAvailableResources();
-        }
-    }
-    
-    function _calculateAvailableResources() constant private returns(uint _amount) {
-        uint8 numBlocksPast = uint8(block.number) - resource.blockNumber;
-        _amount = resource.amount + (numBlocksPast * resource.rate);
-    }
+   
 }
