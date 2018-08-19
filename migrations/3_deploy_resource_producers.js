6a13e1f20e685ec0647867b037038228c18d5fac
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-08-19 15:18:59 +0100

    Adding migration of a resource producer to the build pipeline

--- migrations/3_deploy_resource_producers.js
+++ migrations/3_deploy_resource_producers.js
@@ -2,6 +2,7 @@ const ResourceProducer = artifacts.require("./ResourceProducer.sol");
 const Wood = artifacts.require("./Wood.sol");
 
 module.exports = async function(deployer) {
+    let woodResource = await Wood.deployed();
     console.log(woodResource);
     deployer.deploy(ResourceProducer);
 };
